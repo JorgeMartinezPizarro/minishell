57f507b0182c0da670deb102a16fbfc60bea72f0
Manuel Anguita Trejo <maanguit@c1r8s6.42madrid.com>
2025-12-18 17:00:57 +0100

    added execution.h

---
+++ includes/execution.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maanguit <maanguit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/18 16:50:01 by maanguit          #+#    #+#             */
+/*   Updated: 2025/12/18 16:58:26 by maanguit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EXECUTION_H
+# define EXECUTION_H
+
+#include "minishell.h"
+
+void	exec_b_op(t_tree *tree, e_node_type type);
+void	exec_subprocces(t_tree **tree);
+void	execute_tree(t_tree *tree);
+void	free_tree(t_tree *tree);
+void	exec_pipe(t_tree *tree);
+
+#endif
