18c652613e07236a295a50086d357b5c77c6f4c9
Jorge <jorgemartinezpizarro@gmail.com>
2025-12-21 16:56:08 +0100

    fix control C handler. control C and control D now minics standar shell

--- srcs/main.c
+++ srcs/main.c
@@ -1,15 +1,25 @@
 #include "minishell.h"
 #include "libft.h"
 #include "minishell_jorge.h"
+#include <signal.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
 
-// TODO: usar variable global aqui.
+// exec_tree se comunica directamente con el main, no es 
+// necesario usar una señal para eso. Otra opcion es 
+// pasar la funcion de exit y clean por parametro.
 int	exit_code = 0;
+
+// Variable para identificar la minishell activa, para 
+// reaccionar a control + c como lo hace shell.
+volatile sig_atomic_t g_state = 0;
 
 void sigint_handler(int sign)
 {
-    // TODO: mirar bien el shell level y el isatty para ver que shell debe reaccionar
-	
-	(void)sign;
+    (void)sign;
+	if (g_state != 1)
+        return ;	
     write(1, "\n", 1);
     rl_replace_line("", 0);
     rl_on_new_line();
@@ -35,22 +45,23 @@ int	exec_line(t_list *env, char *line)
 
 char	*get_name(t_list *env)
 {
-	char *var = get_env_value(env, "SESSION_MANAGER");
+	char **vals;
+	char **its;
+	char **els;
+	char *sol;
+	char *var;
+
+	var = get_env_value(env, "SESSION_MANAGER");
 	if (!var)
-		return ft_strdup("unknown");
-	char **vals = ft_split(var, ':');
-
-	char **its = ft_split(vals[0], '.');
-
-	char **els = ft_split(its[0], '/');
-
-	char *sol = ft_strdup(els[1]);
-
+		return (ft_strdup("unknown"));
+	vals = ft_split(var, ':');
+	its = ft_split(vals[0], '.');
+	els = ft_split(its[0], '/');
+	sol = ft_strdup(els[1]);
 	free_str_array(vals);
 	free_str_array(its);
 	free_str_array(els);
-	
-	return sol;
+	return (sol);
 }
 
 int main(int argc, char **args, char **env)
@@ -73,13 +84,19 @@ int main(int argc, char **args, char **env)
 		head = ft_strreplace(tmp, "####", name);
 		free(tmp);
 		free(name);
+		g_state = 1;
 		char *line = readline(head);
-		
+		g_state = 0;
 		while (line)
 		{
-			if (ft_strcmp(line, "") != 0)//si getline siempre devuelve nl sería in
+			g_state = 2;
+			if (ft_strcmp(line, "") != 0) // Ignoramos lineas vacias.
 				exec_line(env_lst, line);
+			g_state = 0;
+			g_state = 1;
 			line = readline(head);
+			g_state = 0;
+
 		}
 		free(head);
 	}
