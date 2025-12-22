84f39257a8775a60350c7ac2431355caf174893f
Jorge Martinez Pizarro <jomarti3@c2r8s3.42madrid.com>
2025-12-22 15:02:22 +0100

    fix some leaks. enhance cd built in with ~ and -

--- srcs/execution/run_cd.c
+++ srcs/execution/run_cd.c
@@ -6,7 +6,7 @@
 /*   By: jomarti3 <jomarti3@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 11:48:53 by jomarti3          #+#    #+#             */
-/*   Updated: 2025/12/21 12:13:22 by jomarti3         ###   ########.fr       */
+/*   Updated: 2025/12/22 14:54:59 by jomarti3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,11 @@
 
 static char	*get_cd_target(t_cmd *com)
 {
-	if (com->args->next == NULL)
-		return (expand_vars(ft_strdup("$HOME"), com->env));
-	return (expand_vars(ft_strdup(com->args->next->str), com->env));
+	if (com->args->next == NULL || ft_strcmp(com->args->next->str, "~") == 0)
+		return (get_env_value(com->env, "HOME"));
+	else if (ft_strcmp(com->args->next->str, "-") == 0)
+		return (get_env_value(com->env, "OLDPWD"));
+	return (com->args->next->str);
 }
 
 static int	change_directory(char *path)
@@ -42,17 +44,17 @@ int	run_cd(t_cmd *com)
 	char	*new_path;
 	char	*old;
 
-	old = getcwd(NULL, 0);
+	old = get_env_value(com->env, "PWD");
 	target = get_cd_target(com);
 	if (!target)
 		return (0);
-	new_path = join_paths(getcwd(NULL, 0), target);
-	free(target);
+	new_path = join_paths(old, target);
 	if (!new_path)
 		return (0);
 	if (!change_directory(new_path))
 		return (free(new_path), 0);
 	set_env_value(&com->env, "OLDPWD", old);
 	set_env_value(&com->env, "PWD", new_path);
+	free(new_path);
 	return (1);
 }
